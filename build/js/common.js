aa401cb298460782f80da6f6747656f53c51d596
Valentin Dorosh <mrdvv@mail.ru>
2016-01-19 02:39:31 +0200

    almost done.

--- build/js/common.js
+++ build/js/common.js
@@ -1,5 +1,34 @@
 $(document).ready(function() {
 
-	// go js
+	function visibleElements() {
+		$('.js-visible').each(function() {
+			if ($(window).scrollTop() + $(window).height() >= $(this).offset().top + ($(this).outerHeight()/2) ) {
+				$(this).addClass('is-visible');
+			};
+		});
+	};
+
+	function sectionFixed() {
+		$('.js-sections').each(function() {
+			if ($(window).scrollTop() + $(window).height()/2 >= $(this).find('.section').first().offset().top + ($(this).find('.section').first().outerHeight()/2)) {
+				$(this).removeClass('bg-scroll');
+				$(this).addClass('bg-fixed');
+				if ($(window).scrollTop() + $(window).height()/2 >= $(this).find('.section').last().offset().top + ($(this).find('.section').last().outerHeight()/2)) {
+					$(this).removeClass('bg-fixed')
+				};
+			}
+			else {
+				$(this).removeClass('bg-fixed');
+			}
+		});
+	}
+
+	visibleElements();
+	sectionFixed();
+
+	$(window).scroll(function() {
+		visibleElements();
+		sectionFixed();
+	});
 
 });
